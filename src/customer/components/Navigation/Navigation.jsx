b9371d7a2d1cd7a6e27e2c29d48a78fe1ced4d8a
“Bhanuka-max” <bhanukaprabhath28@gmail.com>
2025-12-08 17:28:13 +0530

    updated nav bar

--- src/customer/components/Navigation/Navigation.jsx
+++ src/customer/components/Navigation/Navigation.jsx
@@ -290,7 +290,7 @@ export default function Navigation() {
                   <span className="sr-only">Your Company</span>
                   <img
                     alt=""
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=pink&shade=600"
                     className="h-8 w-auto"
                   />
                 </button>
